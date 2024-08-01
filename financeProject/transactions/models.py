988a38d6eeb62e20363d5633a0cc52584153ef63
Omer Erturk <111006230+omerert@users.noreply.github.com>
2024-07-31 23:51:59 -0500

    Created classes for income and spendings

--- financeProject/transactions/models.py
+++ financeProject/transactions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Income(models.model):
+    nameOfIncome = models.CharField(max_length=75)
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.FloatField()
+class Spendings(models.model):
+    nameOfSpending = models.CharField(max_length=75)
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.FloatField()
