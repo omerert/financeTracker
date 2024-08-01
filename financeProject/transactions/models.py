e8156626a4b8c2e00a2b03bd508e6a009aab614a
Omer Erturk <111006230+omerert@users.noreply.github.com>
2024-07-31 23:54:37 -0500

    Migrarted built-in and own models

--- financeProject/transactions/models.py
+++ financeProject/transactions/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Income(models.model):
+class Income(models.Model):
     nameOfIncome = models.CharField(max_length=75)
     date = models.DateTimeField(auto_now_add=True)
     amount = models.FloatField()
-class Spendings(models.model):
+class Spendings(models.Model):
     nameOfSpending = models.CharField(max_length=75)
     date = models.DateTimeField(auto_now_add=True)
     amount = models.FloatField()
