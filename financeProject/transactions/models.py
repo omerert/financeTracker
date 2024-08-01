28be1e6ce8918f8979ff9995a277dd1171bb1ab4
Omer Erturk <111006230+omerert@users.noreply.github.com>
2024-08-01 14:41:02 -0500

    Modified Models.py

--- financeProject/transactions/models.py
+++ financeProject/transactions/models.py
@@ -5,6 +5,10 @@ class Income(models.Model):
     nameOfIncome = models.CharField(max_length=75)
     date = models.DateTimeField(auto_now_add=True)
     amount = models.FloatField()
+
+    def __str__(self):
+        return self.nameOfIncome
+    
 class Spendings(models.Model):
     nameOfSpending = models.CharField(max_length=75)
     date = models.DateTimeField(auto_now_add=True)
