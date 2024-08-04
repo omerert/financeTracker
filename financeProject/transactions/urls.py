a6076508e6ecdd11923743f44752a6dadb9278dc
Omer Erturk <111006230+omerert@users.noreply.github.com>
2024-08-03 21:45:59 -0500

    Combined Income and Expense model into Transaction model

--- financeProject/transactions/urls.py
+++ financeProject/transactions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('income/',views.listIncome),
+    path('', views.listTransaction),
     # path('about/',views.about)
 ]
--- financeProject/transactions/views.py
+++ financeProject/transactions/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from .models import Income, Spending
+from .models import Income, Spending, Transaction
+from django.db.models import Sum
 # Create your views here.
 
 def listIncome(request):
     incomes = Income.objects.all().order_by("-date")
     return render(request, 'income/income.html', {'incomes': incomes})
+def listTransaction(request):
+    transactions = Transaction.objects.all().order_by("-date")
+    totalExpense = Transaction.objects.filter(transactionType='E')
+    total_expense_amount = totalExpense.aggregate(Sum('amount'))['amount__sum']
+    return render(request, 'transactions.html', {'transactions': transactions, 'totalExpense': total_expense_amount})
+
