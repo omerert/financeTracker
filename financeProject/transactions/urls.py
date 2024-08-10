4ce0c0ab948bdaf6158fcd77595c9fc921530e02
Omer Erturk <111006230+omerert@users.noreply.github.com>
2024-08-09 23:38:20 -0500

    Removed Income views

--- financeProject/transactions/urls.py
+++ financeProject/transactions/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('income/',views.listIncome),
     path('', views.listTransaction),
     # path('about/',views.about)
 ]
--- financeProject/transactions/views.py
+++ financeProject/transactions/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from .models import Income, Spending, Transaction
+from .models import Transaction
 from django.db.models import Sum
 # Create your views here.
 
-def listIncome(request):
-    incomes = Income.objects.all().order_by("-date")
-    return render(request, 'income/income.html', {'incomes': incomes})
+
 def listTransaction(request):
     transactions = Transaction.objects.all().order_by("-date")
     expences = Transaction.objects.filter(transactionType='E')
